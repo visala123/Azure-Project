12511541d208a517c957f3f0ba7f61b159046614
visala123 <visala.k@algorims.com>
2025-05-19 19:11:24 +0800

    commit

--- project-root/main.tf
+++ project-root/main.tf
@@ -1,7 +1,7 @@
 module "networking" {
   source = "./networking"
- resource_group_name    = azurerm_resource_group.rg.name
-  resource_group_location = azurerm_resource_group.rg.location
+ resource_group_name    = azurerm_resource_group_name
+  resource_group_location = azurerm_resource_group_location
   vnet_name           = var.vnet_name
   subnet_name         = var.subnet_name
   address_space       = var.address_space
